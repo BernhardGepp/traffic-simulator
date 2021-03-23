32ccf7ce7eca69621871421331af52e984c62eb1
BernhardGepp <75421412+BernhardGepp@users.noreply.github.com>
2021-03-23 16:59:57 +0100

    Update callBackLinks.cpp

--- callBackLinks.cpp
+++ callBackLinks.cpp
@@ -1,16 +1,2 @@
 #include "PrecompiledHeadersEdges.h"
 #include "callBackLinks.h"
-//static void (*m_f10PaintBox)(HDC hdc, const int &param1, const int &param2, const int &param3, const int &param4)=nullptr;
-//static void(*m_f11PaintWhiteLine)(HDC hdc, const int &param1, const int &param2, const int &param3, const int &param4, const int &param5)=nullptr;
-/*bool callBackLinks::instanceFlag = false;
-//std::unique_ptr<callBackLinks>& callBackLinks::instance = 0;
-
-std::unique_ptr<callBackLinks>& callBackLinks::getInstance() {
-	if (!instanceFlag) {
-		single= std::make_unique<callBackLinks>();
-			return single;
-	}
-	else {
-		return single;
-	}
-}*/
