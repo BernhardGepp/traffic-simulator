22d0ec0df11db7c097c27a6c5fbf1f7c5efaad8d
BernhardGepp <75421412+BernhardGepp@users.noreply.github.com>
2021-03-23 17:27:56 +0100

    Update sectionVehicleSet.h

--- sectionVehicleSet.h
+++ sectionVehicleSet.h
@@ -4,30 +4,20 @@
 
 #include "random_speed_neu.h"
 
-//#include "edge.h"
-
-
 class sectionVehicleSet 
 {
-private:
-	
-	
-
-	
-	
+private:	
 	
 public:
 	void(*m_f6PrintLaneInNumbers)(HDC hdc, const int &iPosXLk, const int &iPosYLk, const int &iPosXRk, const int &iPosYRk, const bool &HorV) = nullptr;
 	std::set<vehicle*, boost::indirect_fun<std::less<vehicle>>>m_vehicleSet;
 	int(*m_callback_getRandomNumber)() = nullptr;
 	HDC m_hdc; 
-	//bool m_riseOrDecline = true;
+	
 	explicit sectionVehicleSet::sectionVehicleSet() {
 
 	}
-	/*sectionVehicleSet::sectionVehicleSet(int(*callback_getRandomNumber)(), 
-		void(*f6PrintLaneInNumbers)(HDC hdc, const int &iPosXLk, const int &iPosYLk, const int &iPosXRk, const int &iPosYRk, const bool &HorV))
-		:m_callback_getRandomNumber(callback_getRandomNumber), m_f6PrintLaneInNumbers(f6PrintLaneInNumbers){*/
+	
 	explicit sectionVehicleSet::sectionVehicleSet(int(*callback_getRandomNumber)(),HDC hdc)
 		:m_callback_getRandomNumber(callback_getRandomNumber),m_hdc(hdc) {
 		
@@ -35,20 +25,7 @@ public:
 	sectionVehicleSet::~sectionVehicleSet() {
 		
 	}
-	//sectionVehicleSet::sectionVehicleSet(const sectionVehicleSet &other) = delete;
-	//sectionVehicleSet::sectionVehicleSet(const sectionVehicleSet &other) {
-		/*flag = other.flag;
-		ownSpeed = other.ownSpeed;
-		ownPosition = other.ownPosition;
-		speedAheadVehicleAt1L = other.speedAheadVehicleAt1L;
-		positionAheadVehicleAt1L = other.positionAheadVehicleAt1L;
-		speedAheadVehicleAt2L = other.speedAheadVehicleAt2L;
-		positionAheadVehicleAt2L = other.positionAheadVehicleAt2L;
-		numberOfVehicleinRange = other.numberOfVehicleinRange;
-		for (auto &i:other.m_vehicleSet){
-			m_vehicleSet.insert(i);
-		}*/
-	//}
+	
 	sectionVehicleSet::sectionVehicleSet(const sectionVehicleSet&& other) = delete;
 	virtual void sectionVehicleSet::printContentOfSection(const int& p1xx, const int& p1yy,
 		const int& p2xx, const int& p2yy) = 0;
@@ -64,26 +41,15 @@ public:
 			
 		}
 		else {
-			//std::cout << "Allocation in VehicleSet failed!\n";
+			
 		}
 	}
 	
-
-	
-	/*void sectionVehicleSet::setServiceBoolFalse() {
-		for (auto &i : m_vehicleSet) {
-			i->serviceBool = false;
-		}
-
-	}*/
 	size_t sectionVehicleSet::getVehicleSetSize() const {
 
 		return m_vehicleSet.size();
 
-	}
-	
-
-	
+	}	
 
 	
 	int sectionVehicleSet::sumOfVehicleSpeedInEdge() {
