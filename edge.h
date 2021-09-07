2619e4070c4068e49a81a6bf4ed0a6d59f5db618
BernhardGepp <75421412+BernhardGepp@users.noreply.github.com>
2021-09-07 23:55:08 +0200

    Update edge.h

--- edge.h
+++ edge.h
@@ -9,7 +9,8 @@
 
 class edge {
 private:
-	int m_simulationIterator = 0;	
+	std::vector<vehicle*> vehicleEraseVector;
+	int p1Shape = 99;
 	int p2Shape = 99;
 	HDC m_hdc;
 	void(*m_f8PaintPoint)(HDC hdc, const std::vector<std::tuple<int, int,int>> &PointsToBePrinted) = nullptr;
@@ -17,7 +18,8 @@ private:
 	void(*m_f6PrintLaneInNumbers)(HDC hdc, const int &iPosXLk, const int &iPosYLk, const int &iPosXRk, const int &iPosYRk, const bool &HorV)=nullptr;
 	int (*m_callback_getRandomNumber)()=nullptr;
 	int m_maxVelocity = 140;
-	int m_maxVelocity_Density = 140;	
+	int m_maxVelocity_Density = 140;
+	bool m_risingOrDescention = true;
 	bbe::PoolAllocator<vehicle>* m_VPAptr = nullptr; //Pointer auf den Poolallocator
 	int m_numberOfVehicleinRange = 0;
 public:
@@ -42,12 +44,11 @@ public:
 	float m_density = 0.0f;
 	float m_averageSpeed = 0.0f;
 	float m_averageTravelTime = 0.0f;
-	int m_length = 0;
-	bool m_risingOrDescention = true;
-	int p1Shape = 99;
-	std::vector<vehicle*> vehicleEraseVector;
+	unsigned int m_length = 0;
 	edge::edge() {
+
 	}
+	
 	explicit edge::edge(const int& startVertex, const int& endVertex, const HDC& hdc,std::unique_ptr<PrintPattern>& pp_ptr,
 		int(*callback_getRandomNumber)(),
 		void(*f8PaintPoint)(HDC hdc, const std::vector<std::tuple<int, int, int>>& PointsToBePrinted),
@@ -55,6 +56,7 @@ public:
 		void(*f6PrintLaneInNumbers)(HDC hdc, const int &iPosXLk, const int &iPosYLk, const int &iPosXRk, const int &iPosYRk, const bool &HorV),
 		concreteObserverSubjekt* cOSptr)
 		: m_startVertex(startVertex), m_endVertex(endVertex), m_hdc(hdc),m_ppPtr(std::move(pp_ptr)), m_callback_getRandomNumber(callback_getRandomNumber), m_f8PaintPoint(f8PaintPoint), m_f5PaintLane(f5PaintLane), m_f6PrintLaneInNumbers(f6PrintLaneInNumbers),m_cOSptr(cOSptr) {
+		
 		
 		vehicleEraseVector.reserve(30);
 		m_routeServiceBool = false;
@@ -64,13 +66,14 @@ public:
 		sFs.m_callback_getRandomNumber = callback_getRandomNumber;
 		sFs.m_f6PrintLaneInNumbers = f6PrintLaneInNumbers;
 		m_cOSptr->registrieren(m_observerPTR);
+		
 	}
-	edge::~edge() {
-	}
+	
 	edge::edge(const edge& other) = delete;
-	edge::edge(edge&& other) = default;
+	edge::edge(edge&& other) = default;	
 	edge& operator=(const edge& other) = delete;
 	edge& operator=(edge&& other) = default;
+	
 
 	int edge::getStartVertex() {
 		return m_startVertex;
@@ -101,37 +104,37 @@ public:
 		p2x = m_endVertexPtr->m_XcoordinateVertex;
 		p2y = m_endVertexPtr->m_YcoordinateVertex;
 		p2Shape = m_endVertexPtr->m_shapeOfThatVertex;
-		
-		if (p1x == p2x) {
+		if (p1x == p2x) {//Position Vertical
 			m_numberOfLanes = m_startVertexPtr->m_numberOfLanesVertical;
-		}
-		if (p1y == p2y) {
-			m_numberOfLanes = m_startVertexPtr->m_numberOfLanesHorizontal;
-		}
-		if (p1x == p2x) {
 			m_verticalOrHorizontal = true;
 			if (p1y < p2y) {
 				m_risingOrDescention = true;
-				m_length = p2y - p1y;
-				
+				if ((p2y - p1y) < 0)
+					throw "negative Length!";
+				m_length = static_cast<unsigned int>(p2y) - static_cast<unsigned int>(p1y);
 			}
 			if (p1y > p2y) {
 				m_risingOrDescention = false;
-				m_length = p1y - p2y;
-				
+				if ((p1y - p2y) < 0)
+					throw "negative Length!";
+				m_length = static_cast<unsigned int>(p1y) - static_cast<unsigned int>(p2y);
 			}
 		}
-		if (p1y == p2y) {
+		if (p1y == p2y) {//Position Horizontal
+			m_numberOfLanes = m_startVertexPtr->m_numberOfLanesHorizontal;
 			m_verticalOrHorizontal = false;
 			if (p1x < p2x) {
 				m_risingOrDescention = true;
-				m_length = p2x - p1x;
+				if ((p2x - p1x) < 0)
+					throw "negative Length!";
+				m_length = static_cast<unsigned int>(p2x) - static_cast<unsigned int>(p1x);
 				
 			}
 			if (p1x > p2x) {
 				m_risingOrDescention = false;
-				m_length = p1x - p2x;
-				
+				if ((p1x - p2x) < 0)
+					throw "negative Length!";
+				m_length = static_cast<unsigned int>(p1x) - static_cast<unsigned int>(p2x);
 			}
 		}
 		sFs.setEndingPoints(m_startVertexPtr->m_XcoordinateVertex, m_startVertexPtr->m_YcoordinateVertex, m_endVertexPtr->m_XcoordinateVertex, m_endVertexPtr->m_YcoordinateVertex, m_maxVelocity, m_maxVelocity_Density);
@@ -165,7 +168,6 @@ public:
 
 
 	void edge::simiRun(const int& simulationIterator) {
-		m_simulationIterator = simulationIterator;
 		
 		for (auto &i : sFs.vehicleSetPtr->m_vehicleSet) {
 			
@@ -173,40 +175,36 @@ public:
 			
 		}
 		if (m_numberOfLanes == 1) {
-			flow1L(1,220);
-			singleSimulationStep(220);
+			flow1L(1,220);				//Insertion of new vehicles into the edge
+			singleSimulationStep(220);	//Treatment of all vehicles in the edge according the traffic flow simulation alg.
 		}
 		if (m_numberOfLanes == 2) {
-			flow1L(1,440);
-			singleSimulationStep(440);
+			flow1L(1,440);				//Insertion of new vehicles into the edge
+			singleSimulationStep(440);	//Treatment of all vehicles in the edge according the traffic flow simulation alg.
 		}
-		if (m_observerPTR->m_position.first > 0) {//Wenn Observer aktiviert wurde!
+		if (m_observerPTR->m_position.first > 0) {//Possible blocking of the Link
 			allocateVehicleAtPositionX();
 		}
 		if (simulationIterator % 10 == 0) {
 			computeEdgesCharactaristics();
 		}
 		sFs.vehicleSetPtr->printContentOfSection(m_startVertexPtr->m_XcoordinateVertex, m_startVertexPtr->m_YcoordinateVertex, m_endVertexPtr->m_XcoordinateVertex, m_endVertexPtr->m_YcoordinateVertex);
-		
+		//Print of the simulation result
 	}
 
 
 	void edge::flow1L(const int& a,const int& b) {
-
+		//Insertion of new vehicles into the edge
 		bool checkIfPositionIsEmpty = false;
 		//************************************************************
 		//Prüfung ob erste Position im Set belegt ist!
 		for (auto &i : sFs.vehicleSetPtr->m_vehicleSet) {
 			i->m_riseOrDecline = m_risingOrDescention;
-			if ((i->m_position == 0) && (m_risingOrDescention == true)) {
-				//std::cout << "Allocation failed!\n";
+			if ((i->m_position == 0) && (m_risingOrDescention == true)) {				
 				checkIfPositionIsEmpty = true;
-
 			}
 			if ((i->m_position == m_length) && (m_risingOrDescention == false)) {
-				//std::cout << "Allocation failed!\n";
 				checkIfPositionIsEmpty = true;
-
 			}
 		}
 		if (checkIfPositionIsEmpty == false) {
@@ -229,9 +227,30 @@ public:
 			}
 		}
 	}
+	vehicle* edge::insertion(vehicle* VPAEptr) {
+		//Preparation of the objects which will be inserted into the edge
+		if (VPAEptr != nullptr) {
+			VPAEptr->setPtr(VPAEptr);
+			VPAEptr->m_moblieORStationary = true;
+			VPAEptr->m_riseOrDecline = m_risingOrDescention;
+			VPAEptr->m_inRange = true;
+			VPAEptr->m_lane = 1;
+			VPAEptr->serviceBool = false;
+			//VPAEptr->processedByIteration = false;
+			if (m_risingOrDescention == true) {
+				VPAEptr->m_position = 0;
+			}
+			if (m_risingOrDescention == false) {
+				VPAEptr->m_position = m_length;
+			}
+			return VPAEptr;
 
+		}
+		return nullptr;
+	}
 	
-	void edge::singleSimulationStep(const int& param) {
+	void edge::singleSimulationStep(const unsigned int& param) {
+		//Treatment of all vehicles in the edge
 		size_t vehicleSetSize = 0;
 		vehicleSetSize = sFs.vehicleSetPtr->getVehicleSetSize();
 		size_t counter = 0;
@@ -254,8 +273,7 @@ public:
 		
 	}
 	
-	void edge::sort() {
-		
+	void edge::sort() {		
 		
 		std::vector <vehicle*> vehicleVector;
 		for (auto i : sFs.vehicleSetPtr->m_vehicleSet) {
@@ -272,8 +290,6 @@ public:
 		VPAEptr->m_routeVertexID_vehicle.clear();
 
 		if (!m_routeTable_IDrValueIDvertex.empty()) {
-
-
 			
 			if ((m_routeTableIterator >= 0) && (m_routeTableIterator < m_routeTable_IDrValueIDvertex.size())) {
 
@@ -293,8 +309,6 @@ public:
 				for (size_t i = 0; i < m_routeTable_IDrValueIDvertex[m_routeTableIterator].second.size(); i++) {
 					VPAEptr->m_routeVertexID_vehicle.push_back(m_routeTable_IDrValueIDvertex[m_routeTableIterator].second[i]);
 				}
-
-
 				m_routeTableIterator++;
 				if (m_routeTableIterator >= m_routeTable_IDrValueIDvertex.size()) {
 					m_routeTableIterator = 0;
@@ -309,33 +323,12 @@ public:
 		}
 		return VPAEptr;
 	}
-	vehicle* edge::insertion(vehicle* VPAEptr) {
-		if (VPAEptr != nullptr) {
-			VPAEptr->setPtr(VPAEptr);
-			VPAEptr->m_moblieORStationary = true;
-			VPAEptr->m_riseOrDecline = m_risingOrDescention;
-			VPAEptr->m_inRange = true;
-			VPAEptr->m_lane = 1;
-			VPAEptr->serviceBool = false;
-			//VPAEptr->processedByIteration = false;
-			if (m_risingOrDescention == true) {
-				VPAEptr->m_position = 0;
-			}
-			if (m_risingOrDescention == false) {
-				VPAEptr->m_position = m_length;
-			}
-			return VPAEptr;
-
-		}
-		return nullptr;
-	}
+	
 
 	
-	void edge::allocateVehicleAtPositionX(){//const int& xCoordinate, const int& yCoordinate) {
-		bool checkIfPositionIsEmpty = false;
-		
-		
-		
+	void edge::allocateVehicleAtPositionX(){
+		//The method is executed when an edge should to be blocked at a certain position
+		bool checkIfPositionIsEmpty = false;		
 		if (m_observerPTR->m_position.second>0) {
 			for (auto &i : sFs.vehicleSetPtr->m_vehicleSet) {
 				if (i->m_position == m_observerPTR->m_position.first) {
@@ -385,7 +378,7 @@ public:
 
 
 	void edge::deallocateVehicleAtEnd(bool totalRelease) {
-		
+		//This method removes vehicles from the edge, which have already passed it
 		size_t a = 0;
 		
 		vehicleEraseVector.clear();
