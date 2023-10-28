860c7f7fff295bc6b5070135e650c840585a0a94
BernhardGepp <gepp@gmx.at>
2023-10-28 19:44:11 +0200

    Test cases changed

--- CMBuild/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/bernh/Documents/25_Sommer2022/CMBuild/_deps/googletest-src/googletest
-# Build directory: C:/Users/bernh/Documents/25_Sommer2022/CMBuild/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
