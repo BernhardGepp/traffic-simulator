cc502c2111be3ba5e2deb091e0c34d0630dcfd73
BernhardGepp <gepp@gmx.at>
2023-10-28 17:13:52 +0200

    .gitignore changed afterwards

--- CMBuild/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/bernh/Documents/25_Sommer2022/googletest
-# Build directory: C:/Users/bernh/Documents/25_Sommer2022/CMBuild/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/googletest-build")
